48c1eb6c4e83ff4d9e8fcca82360dadfa34bcb1f
Vishwajeet Patil <Vishwajeet.patil@zerovaega.in>
2023-07-21 08:35:34 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_day1/home_screen.dart';
+import 'package:flutter_application_day1/screens/login_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter_application_day1/screens/home_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +13,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: HomeScreen(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const LoginScreen(),
+        "/home": (context) => const HomeScreen(),
+        "/login": (context) => const LoginScreen()
+      },
+    );
   }
 }
